ebc146c48f639653d3c2bc664583fd6b0e153215
kei umemoto <keikeikun@icloud.com>
2025-01-03 23:35:42 +0900

    どうや

--- mynovelsite/settings.py
+++ mynovelsite/settings.py
@@ -228,8 +228,8 @@ INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
 # INTERNAL_IPS = ['127.0.0.1', '192.168.10.1']  # 192.168.10.1はDockerホストのIPアドレスの例
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://redis:6379/0')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
