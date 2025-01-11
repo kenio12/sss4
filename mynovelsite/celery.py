efdbd37a2821a28667cc20f68cd997a45f34e3f5
kei umemoto <keikeikun@icloud.com>
2025-01-11 17:47:34 +0900

    どうや

--- mynovelsite/celery.py
+++ mynovelsite/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
-from redis.connection import SSLConnection
+import ssl
 
 # Django設定モジュールを指定
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mynovelsite.settings')
@@ -12,10 +12,20 @@ app = Celery('mynovelsite')
 # Redis URL設定
 redis_url = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
 
+# SSL設定
+ssl_settings = {
+    'ssl_cert_reqs': ssl.CERT_NONE,
+    'ssl_ca_certs': None,
+    'ssl_certfile': None,
+    'ssl_keyfile': None
+}
+
 # Celery設定
 app.conf.update(
     broker_url=redis_url,
     result_backend=redis_url,
+    broker_use_ssl=ssl_settings,
+    redis_backend_use_ssl=ssl_settings,
     timezone='Asia/Tokyo',
     enable_utc=True,
     beat_schedule={
@@ -39,13 +49,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # タスクの自動検出
 app.autodiscover_tasks()
-
-# SSLの設定
-if app.conf.broker_url.startswith('rediss://'):
-    app.backend.connparams.update({
-        'ssl_cert_reqs': None,
-        'connection_class': SSLConnection
-    })
 
 # アプリケーションのエクスポート
 celery = app
