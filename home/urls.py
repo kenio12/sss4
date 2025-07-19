b226f018a4df5cce1e52bbf16498c041aa2d96ef
kei umemoto <keikeikun@icloud.com>
2025-07-19 10:30:15 +0900

    ショートカット配布ページのURL設定追加

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import HomePageView, novels_list_ajax
 from . import views
 app_name = 'home'
@@ -9,4 +10,5 @@ urlpatterns = [
     path('terms/', views.terms, name='terms'),
     path('main', views.main_view, name='main_view'),
     path('admin-home/', views.admin_home, name='admin_home'),
+    path('shortcut_guide.html', TemplateView.as_view(template_name='shortcut_guide.html'), name='shortcut_guide'),
 ]
