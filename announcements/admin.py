1cdfec6f2d3ad94fc041af5f6f62dc5e34b4f0b6
kei umemoto <keikeikun@icloud.com>
2025-12-06 14:13:09 +0900

    📝 管理画面アナウンス作成者をニックネーム表示に変更

--- announcements/admin.py
+++ announcements/admin.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
+from django import forms
 from .models import Announcement
+from accounts.models import User
+
+
+class AnnouncementAdminForm(forms.ModelForm):
+    """作者選択でニックネームを表示するカスタムフォーム"""
+    created_by = forms.ModelChoiceField(
+        queryset=User.objects.filter(is_staff=True),
+        label='作成者',
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # ニックネームで表示するようにlabel_from_instanceをオーバーライド
+        self.fields['created_by'].label_from_instance = lambda obj: obj.nickname or obj.username
+
+    class Meta:
+        model = Announcement
+        fields = '__all__'
+
 
 @admin.register(Announcement)
 class AnnouncementAdmin(admin.ModelAdmin):
+    form = AnnouncementAdminForm
     list_display = ('title', 'is_pinned', 'created_at', 'is_active')
     list_editable = ('is_pinned', 'is_active')
     ordering = ('-is_pinned', '-created_at')
-    
+
     def save_model(self, request, obj, form, change):
         if not obj.created_by:
             obj.created_by = request.user
